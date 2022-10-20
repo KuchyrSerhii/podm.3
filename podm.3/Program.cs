f81816376538f00bba8cc1aff130ff6e59944b70
ou7pc18 <ou7pc18@OU7PC18>
2022-10-20 11:23:56 +0200

    Přidejte soubory projektu.

---
+++ podm.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace podm._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zadejte číslo k odmocnění");
+            string z_klavesnice = Console.ReadLine();
+            double k_odmocneni = Convert.ToDouble(z_klavesnice);
+            if (k_odmocneni > 0)
+                Console.WriteLine("Zadal jsi číslo větší než 0 a já ho můžu odmocnit");
+            double odmocnina = Math.Sqrt(k_odmocneni);
+            Console.WriteLine("odmocnina z cisla  " + k_odmocneni + " je " + odmocnina);
+            if (k_odmocneni <= 0)
+                Console.WriteLine("Odmocnina ze záporného čísla neexistuje!");
+            Console.ReadKey();
+        }
+    }
+}
